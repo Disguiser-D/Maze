6059b71d6fb21c91ccfdacf00cbe5a772de64c0c
CyiceK <klvrw@outlook.com>
2021-03-11 22:17:09 +0800

    迷宫与栈 非递归代码头文件补充

--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@
 
 #include "include/loadmaze.h"
 #include "include/allmazeaccess.h"
+#include "include/non_recursive.h"
 
-int main()
-{
-
+int main() {
     int **b = CreateMaze("333.txt");
     if (b == nullptr) return -1;
-    RunAllAccessDFS(LoadStartX("333.txt"),LoadStartY("333.txt"),LoadEndX("333.txt"),LoadEndY("333.txt"), b);
+    RunAllAccessDFS(LoadStartX("333.txt"), LoadStartY("333.txt"), LoadEndX("333.txt"), LoadEndY("333.txt"), b);
 }
 
